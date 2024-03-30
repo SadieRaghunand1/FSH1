065ef3bf81c5a4d3a43daa404ecf996ebdc95ab3
SadieRaghunand1 <sadielrowl@outlook.com>
2024-03-30 15:34:27 -0400

    A fear driven commit

--- Fsh1/Assets/Scripts/NonPlayerCharacter.cs
+++ Fsh1/Assets/Scripts/NonPlayerCharacter.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NonPlayerCharacter : MonoBehaviour
 {
     public float displayTime = 5f;
     public GameObject dialogBox;
-    private float timerDisplay;
+    public string[] dialog;
+    public TextMeshProUGUI dialogText;
+    public float textSpeed;
+    private int index;
 
     // Start is called before the first frame update
     void Start()
     {
         dialogBox.SetActive(false);
+        dialogText.text = string.Empty;
     }
 
     // Update is called once per frame
@@ -22,15 +27,45 @@ public class NonPlayerCharacter : MonoBehaviour
 
     public void DisplayDialog()
     {
-        StartCoroutine(TimeDialogue());
+        StartDialog();
+        
     }
 
+    public void StartDialog()
+    {
+        index = 0;
+        StartCoroutine(TypeString());
+    }
+
+    public void NextLine()
+    {
+        if(index < dialog.Length - 1) 
+        {
+            index++;
+            dialogText.text = string.Empty;
+            StartCoroutine(TypeString());
+        }
+        else
+        {
+            StartCoroutine(TimeDialogue());
+        }
+    }
     public IEnumerator TimeDialogue()
     {
-        dialogBox.SetActive(true);
+        
 
         yield return new WaitForSeconds(displayTime);
 
         dialogBox.SetActive(false);
+    }
+
+    public IEnumerator TypeString()
+    {
+        dialogBox.SetActive(true);
+        foreach (char c in dialog[index].ToCharArray())
+        {
+            dialogText.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
     }
 }
